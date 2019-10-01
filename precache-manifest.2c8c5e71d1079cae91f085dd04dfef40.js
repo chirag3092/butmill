a06f0a267500d1f89aa3ff74668d93b4a2439b8e
Chirag  Patel <chirag.p@peopleinteractive.in>
2019-10-01 12:26:13 +0530

    Updates

---
+++ precache-manifest.2c8c5e71d1079cae91f085dd04dfef40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ca809942236ca9e8821cd59638c5aac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f16c21ad63b97e07adc",
+    "url": "/static/css/main.7f2b6efd.chunk.css"
+  },
+  {
+    "revision": "d5f61cdfee9a23253422",
+    "url": "/static/js/2.3a2ba806.chunk.js"
+  },
+  {
+    "revision": "8f16c21ad63b97e07adc",
+    "url": "/static/js/main.039f70c1.chunk.js"
+  },
+  {
+    "revision": "3c6aa1f2b3013ad3b4c6",
+    "url": "/static/js/runtime~main.dfc097e4.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  }
+]);
